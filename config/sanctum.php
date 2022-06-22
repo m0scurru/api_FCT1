82ebdb0265d0f3f53a6961a7b2be740268cc05a9
m0scurru <57139239+m0scurru@users.noreply.github.com>
2022-06-22 12:32:29 +0200

    .

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,http://fct.filler.vdg:8000,http://192.168.100.150:8000,http://fct.filler.vdg,http://192.168.100.150',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,fct.filler.vdg:8000,192.168.100.151:8000,fct.filler.vdg,192.168.100.151',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
